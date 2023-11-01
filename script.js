67b5db9b588c739598f4c2d8024032fbcd8dffc1
SalisMaxima <112969827+SalisMaxima@users.noreply.github.com>
2023-11-01 14:06:25 +0100

    Simon_exam_V_2_0

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
 // Set the target date and time for the countdown
-const targetDate = new Date('2023-11-10T00:00:00').getTime();
+const targetDate = new Date('2023-11-09T12:00:00').getTime();
 
-// Update the countdown every second
-const countdown = setInterval(function() {
+// Define an array of image sources
+const imageSources = [
+    'Simon_exam_12_5.png',
+    'Simon_exam_25_0.png',
+    'Simon_exam_37_5.png',
+    'Simon_exam_50_0.png',
+    'Simon_exam_62_5.png',
+    'Simon_exam_75_0.png',
+    'Simon_exam_87_5.png',
+    'Simon_exam_100_0.png'
+];
+
+// Function to update the countdown and images
+function updateCountdownAndImages() {
     const now = new Date().getTime();
     const timeRemaining = targetDate - now;
 
     if (timeRemaining <= 0) {
         clearInterval(countdown);
         document.getElementById('countdown').innerHTML = 'Simon has arrived!';
+        // Display the final image when the timer reaches 0 days
+        document.querySelector('.countdown-container img').src = imageSources[7];
     } else {
         const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
         const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
+        // Update the countdown text
         document.getElementById('countdown').innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+        // Update the image based on the number of days left
+        if (days >= 1 && days <= 7) {
+            document.querySelector('.countdown-container img').src = imageSources[7 - days];
+        }
     }
-}, 1000);
+}
+
+// Call the updateCountdownAndImages function initially
+updateCountdownAndImages();
+
+// Update the countdown and images every second
+const countdown = setInterval(updateCountdownAndImages, 1000);
