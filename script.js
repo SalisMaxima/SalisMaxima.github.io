300cd202341214290c665436d13b9b0f8344b45d
SalisMaxima <112969827+SalisMaxima@users.noreply.github.com>
2023-11-05 08:59:36 +0100

    Fuldstændig fri nu

--- script.js
+++ script.js
@@ -23,7 +23,20 @@ function updateCountdownAndImages() {
         document.getElementById('countdown').innerHTML = 'Simon has arrived!';
         // Display the final image when the timer reaches 0 days
         document.querySelector('.countdown-container img').src = imageSources[7];
+        // Check if a video container already exists
+        let videoContainer = document.getElementById('videoContainer');
+        if (!videoContainer) {
+            // Create a new video container if it doesn't exist
+            videoContainer = document.createElement('div');
+            videoContainer.id = 'videoContainer';
+            document.body.appendChild(videoContainer);
+            // Set the innerHTML of the video container
+            videoContainer.innerHTML = `
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/2PrYHNXRdB0?autoplay=1&mute=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        `;
+        }
     } else {
+        // Continue updating the countdown and images
         const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
         const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
@@ -35,6 +48,9 @@ function updateCountdownAndImages() {
         // Update the image based on the number of days left
         if (days >= 1 && days <= 7) {
             document.querySelector('.countdown-container img').src = imageSources[7 - days];
+            
+
+            
         }
     }
 }
